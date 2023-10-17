14920d89fbc8fba9d0faa459b8f7169c9b9273c0
Brayan Hernandez <brayannhc07@gmail.com>
2023-10-17 12:11:32 -0600

    add rules psr-12 and force error

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,15 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function sampleFunction(int $a, int $b = null): array
+    {
+        if ($a === $b) {
+             return [$a, $b]; // Comment
+        } elseif ($a > $b) {
+            return [$a];
+        } else {
+            return [];
+        }
+    }
 }
